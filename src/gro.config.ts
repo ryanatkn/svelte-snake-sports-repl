54690a908c5bd0c878b1747deae32fb66166d2af
Ryan Atkinson <mail@ryanatkn.com>
2022-10-25 13:51:57 -0500

    comment

--- src/gro.config.ts
+++ src/gro.config.ts
@@ -5,7 +5,7 @@ const files = ['lib/index.ts', 'lib/style.css'];
 export const config: GroConfigCreator = async ({dev}) => {
 	const partial: GroConfigPartial = {
 		builds: [dev ? null : {name: 'library', platform: 'node', input: files}],
-		types: false,
+		types: false, // right now this is the primary thing we do for REPL compatibility
 	};
 	return partial;
 };
